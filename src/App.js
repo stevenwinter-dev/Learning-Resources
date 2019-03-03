e3a9dbf93b68b20dea9df8e2d7f50ea0d4e15a42
Steven Winter <stevenwinter.me@gmail.com>
2019-03-02 21:23:53 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Course from './components/Course';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Learning Resources</h1>
+        <p>These are some of the courses, tutorials, and guides I have used in my studies to become a web developer.</p>
+        <Course />
       </div>
     );
   }
---
+++ src/components/Course.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const course = ( props ) => {
+    return(
+        <p>Course 1</p>
+    )
+};
+
+export default course;
